cc18ab8f888e61ecd1ad0a3437dbb30f28dfd7c4
bobby lin <linzhangsheng23@gmail.com>
2023-11-24 16:22:25 +0800

    chore: add vueuse

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
+<script setup lang="ts">
+const { x, y } = useMouse()
+</script>
+
 <template>
   <div text-2xl text-rose>
     Hello World
+    <div>
+      {{ x }}, {{ y }}
+    </div>
   </div>
 </template>
